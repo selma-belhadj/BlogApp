50d1b0992dd18a4840a6384a787c935d2ad7685c
selma <belhadjselma1@gmail.com>
2022-07-09 09:05:26 +0100

    add validation for methods of user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,11 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.update_posts_counter
+  def update_posts_counter
     user.update(posts_counter: user.post.count)
   end
 
-  def self.five_recent_comments
+  def five_recent_comments
     comment.includes(:post).order(created_at: :desc).limit(5)
   end
 end
