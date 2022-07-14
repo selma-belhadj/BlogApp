9347ff831bd7901eed9fa0c7c8ad74ddce10a232
selma <belhadjselma1@gmail.com>
2022-07-14 10:44:07 +0100

    Add tests for posts index and show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) {get user_posts_path(1)}
+
+    it 'responds with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders posts list view' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'shows correct content in the view' do
+      expect(response.body).to include('Here is a list of all posts for a given user based on the given user_id')
+    end
+  end
+
+  describe "GET /index" do
+    before(:example) {get user_post_path(1,1)}
+
+    it 'responds with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders posts list view' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'shows correct content in the view' do
+      expect(response.body).to include('Here is the details of the given post for the given user based on user_id and post_id')
+    end
   end
 end
