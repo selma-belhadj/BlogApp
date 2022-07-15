115ffca842a2429f69c7cca0b777b17f3ac7e1c1
selma <belhadjselma1@gmail.com>
2022-07-16 00:29:50 +0100

    add the show method for the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,8 @@ class PostsController < ApplicationController
     @posts = Post.includes(:user).where(user: params[:user_id]) 
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @last_five_comments = @post.five_recent_comments 
+  end
 end
