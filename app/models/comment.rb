01b69285bf750f5ce4dd8b769b9f1cb01aaff290
selma <belhadjselma1@gmail.com>
2022-07-16 00:31:05 +0100

    add the class_name, foreign_key to the like & comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
+  belongs_to :post, class_name: 'Post'
   after_save :update_comments_counter
 
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user, class_name: 'User', foreign_key: :user_id
+  belongs_to :post, class_name: 'Post'
   after_save :update_likes_counter
 
   def update_likes_counter
