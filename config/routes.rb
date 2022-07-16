25ce0de566d75c8fb4adcd33ef5718f4a18365b0
selma <belhadjselma1@gmail.com>
2022-07-16 12:12:45 +0100

    update the routes comments & likes to be nested inside the posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
-    resources :comments, only: [:create]
-    resources :likes, only: [:create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
 end
