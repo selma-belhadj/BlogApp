97347058f1905a409a50488265e767083f1b1e17
selma <belhadjselma1@gmail.com>
2022-07-16 09:27:07 +0100

    create the current class and assign the user as the attribute

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+    def create
+        @user = 
+        @like = Like.create(like_params)
+        @like.user = Current.user
+        post = Post.find(params[:post_id])
+        @like.post = post
+        if @like.save
+            flash[:notice] = "Like created successfully"
+            redirect_to user_post_path(post.user, post)
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :id)
+    end
 end
