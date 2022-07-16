83424d8cb297e197e9114a14e2204beba93759a7
selma <belhadjselma1@gmail.com>
2022-07-16 06:49:26 +0100

    implement the create method for the comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,22 @@
 class CommentsController < ApplicationController
-    
+    def create
+        @comment = Comment.create(comment_params)
+        @comment.user = Current.user
+        post = Post.find(params[:post_id])
+        @comment.post = post
+        if @comment.save
+            flash[:notice] = "Comment created successfully"
+            redirect_to user_post_path(post.user, post)
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:text)
+    end
+
 end
+
