01fc7a43f5488aef1bb2f053c1b82ae9b36d54f7
selma <belhadjselma1@gmail.com>
2022-07-20 23:27:36 +0100

    Update the migration add_default0_to_posts_counter_for_user

--- db/migrate/20220715111338_add_default0_to_posts_counter_for_user.rb
+++ db/migrate/20220715111338_add_default0_to_posts_counter_for_user.rb
@@ -1,5 +1,6 @@
 class AddDefault0ToPostsCounterForUser < ActiveRecord::Migration[7.0]
   def change
     change_column_default :users, :posts_counter, from: nil, to: 0
+    # change_column_default :users, :posts_counter, 0
   end
 end
