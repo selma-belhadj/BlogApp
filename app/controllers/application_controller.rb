db9a2720b42ee57d4e8d9820a34f82f1a61c6a6f
selma <belhadjselma1@gmail.com>
2022-07-20 23:35:10 +0100

    Update the application controller by adding configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  # before_action :set_current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def set_current_user
-  #   Current.user = User.find_by(id: params[:user_id])
-  # end
+  protected
+
+  def configure_permitted_parameters
+    attributes = %i[name email password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 end
