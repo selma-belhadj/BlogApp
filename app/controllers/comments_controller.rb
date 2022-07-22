a76bb34765a371ffe2771db89f66a7ed86231b77
Ezekiel Utshudi Eteta <ezekielutshudi@gmail.com>
2022-07-22 18:31:01 +0200

    Add destroyer method
    The ability to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    @comment.user = Current.user
+    @comment.user = current_user
     post = Post.find_by(id: params[:post_id])
     @comment.post = post
     if @comment.save
@@ -12,9 +12,11 @@ class CommentsController < ApplicationController
     end
   end
 
-  private
-
-  def comment_params
-    params.permit(:text)
+  def destroy
+    @post = Post.find_by(id: params[:post_id])
+    @comment = @post.comment.find(params[:id])
+    @user = current_user
+    @post.comments_counter -= 1
+    @comment.destroy!
+    redirect_to user_post_path(@user.id, @post.id), notice: 'Comment was successfully deleted.'
   end
-end
