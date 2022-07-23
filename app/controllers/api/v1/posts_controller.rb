8185b7b80e3db72a533751efc8d2df39fe575231
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-07-23 08:25:56 +0200

    add authentication

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,12 +1,29 @@
-class Api::V1::PostsController < ApplicationController
-  # before_action :authenticate_user!
-  def index
-    @posts = Post.all
+require_relative '../concerns/json_tokens'
 
-    render json: @posts, status: :ok
+class Api::PostsController < ApplicationController
+  def index
+    return unless check_auth?(params[:user_id])
+
+    @user = User.find(params[:user_id])
+    return unless @user.token?
+
+    decoded_token = TokenAuthorization.jwt_decode(@user.token)[0]['email']
+    unless decoded_token == @user.email
+      render json: { error: 'You are not authorized !!!' }, status: :unauthorized
+      nil
+    end
+    @posts = Post.where(author_id: params[:user_id])
+    render json: { success: true, data: { posts: @posts } }
   end
 
-  def show
-    @post = Post.find(params[:id])
+  private
+
+  def check_auth?(user_id)
+    user = User.where(id: user_id)
+    if user.empty?
+      render json: { error: 'You are not authorized !!!' }, status: :unauthorized
+      return false
+    end
+    true
   end
 end
