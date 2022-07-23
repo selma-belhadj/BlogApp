4001354d560165efb48bd325bbf8ce77b35b6638
selma <belhadjselma1@gmail.com>
2022-07-23 11:09:24 +0100

    update the routes file with the Api scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions, registrations: :registrations}
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
