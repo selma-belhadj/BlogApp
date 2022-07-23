923861e71f8cfcb8ce3cdbc656b20fdf0347a190
selma <belhadjselma1@gmail.com>
2022-07-23 11:04:22 +0100

    Add the routes for the API

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+
+  get '/api/users/:user_id/posts', to: 'api#user_posts', as: 'api_user_posts'
+  get '/api/users/:user_id/posts/:post_id/comments', to: 'api#user_post_comments', as: 'api_posts_comments'
+  post '/api/users/:user_id/posts/:post_id/comments', to: 'api#new_comment', as: 'new_post_comment'
 end
