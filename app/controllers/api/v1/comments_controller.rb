8d2fd0ddaa5297e0be1142045ceba0bc1a447784
Ezekielutshudi <ezekielutshudi@gmail.com>
2022-07-23 08:41:59 +0200

    add authentication and fix linters

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,27 +1,57 @@
-class Api::V1::CommentsController < ApplicationController
-  # before_action :authenticate_user!
+require_relative '../concerns/json_tokens'
+
+class Api::CommentsController < ApplicationController
   def index
-    @comments = Comment.all
+    return unless check_auth?(params[:user_id])
 
-    render json: @comments, status: :ok
-  end
+    @user = User.find(params[:user_id])
+    return unless @user.token?
 
-  def show
-    @comment = Comment.find(params[:id])
+    decoded_token = TokenAuthorization.jwt_decode(@user.token)[0]['email']
+    if decoded_token == @user.email
+      @comments = Comment.where({ post_id: params[:post_id] }).order('created_at')
+      render json: { success: true, data: { comments: @comments } }, status: :ok
+    else
+      render json: { error: 'You are not authorized !!!' }, status: :unauthorized
+    end
   end
 
   def create
-    @comment = Comment.new(comment_params)
+    return unless check_auth?(params[:user_id])
+
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+
+    return unless @user.token?
+
+    decoded_token = TokenAuthorization.jwt_decode(@user.token)[0]['email']
+    unless decoded_token == @user.email
+      render json: { error: 'You are not authorized !!!' }, status: :unauthorized
+      nil
+    end
+    @comment = Comment.create(author: @user, post: @post, text: comment_params['text'])
     if @comment.save
-      render json: @comment, status: :ok
+      render json: { success: true, data: { comment: @comment } }, status: :created
     else
-      render json: comment.errors, status: :unprocessable_entity
+      render json: { success: false, errors: @comment.errors }, status: :bad_request
     end
   end
 
   private
 
+  def check_auth?(user_id)
+    user = User.where(id: user_id)
+    if user.empty?
+      render json: { error: 'You are not authorized !!!' }, status: :unauthorized
+      return false
+    end
+    true
+  end
+
   def comment_params
-    params.permit(:text, :post_id, :user_id)
+    params.require(:comment).permit(:text)
   end
 end
+white_check_mark
+eyes
+raised_hands
